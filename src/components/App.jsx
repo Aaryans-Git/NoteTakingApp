925935e0587712a245f6c857a099e4d7e93d4d09
Aaryan Pawar <97723245+aaryanpawar8@users.noreply.github.com>
2023-02-14 22:13:30 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>DevTowns</h1>
+      <p>A place to learn and grow at your own pace</p>
+    </div>
+  );
+}
+
+export default Notes;
